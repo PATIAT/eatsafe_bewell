a0d5853b5f650b625bfe603fe3d023eda124ba93
Paul Trotter <paul.trotter9@gmail.com>
2023-03-13 19:02:18 +0000

    Added register template and functionality as well as developing custom styles for the page

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -17,12 +18,51 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+"""
+get_reports() method used to display the reports template to the user upon
+visiting the site.
+"""
+
 
 @app.route("/")
 @app.route("/get_reports")
 def get_reports():
     reports = mongo.db.reports.find()
     return render_template("reports.html", reports=reports)
+
+
+"""
+register() method used to display the register template to the site and to
+gather the user input data from the register form and post it to the database.
+"""
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # checking whether the user name from form already exists
+        # in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This username already exists, please try again!")
+            return redirect(url_for('register'))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "fname": request.form.get("fname").lower(),
+            "lname": request.form.get("lname").lower(),
+            "county": request.form.get("county").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # putting the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Your registration has been successful!")
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
