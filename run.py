f26e83f6dd759a413ae7ced03ba4665d81d0a895
Paul Trotter <paul.trotter9@gmail.com>
2023-03-17 15:03:20 +0000

    Added get_categories() method and categories.html template

--- run.py
+++ run.py
@@ -260,6 +260,18 @@ def delete_report_confirmed(report_id):
         return redirect(url_for("get_reports"))
 
 
+"""
+The get_categories() method find the categories from the database and renders
+them to the template for the user to view.
+"""
+
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
